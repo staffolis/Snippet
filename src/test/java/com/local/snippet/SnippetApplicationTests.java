0589c765ef271eaa61571f58b6ba4f3d874e55b3
staffolis <simone.staffoli@locatee.ch>
2021-03-26 22:24:11 +0100

    Init Snippet Organizer

---
+++ src/test/java/com/local/snippet/SnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.snippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
